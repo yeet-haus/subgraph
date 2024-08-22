93734fdfba71b5db007e4de869e8b9970e542215
skuhlmann <samkuhlmann@gmail.com>
2024-08-22 09:38:49 -0600

    preps mainnet and more ignore

--- manifests/config.js
+++ manifests/config.js
@@ -1,7 +1,23 @@
 module.exports.config = {
   mainnet: {
-    dataSources: [],
-    templates: [],
+    dataSources: [
+      {
+        name: "summoner",
+        template: "summoner-ds.yaml",
+        address: "0x7e988A9db2F8597735fc68D21060Daed948a3e8C",
+        startBlock: 16028392,
+      },
+    ],
+    templates: [
+      {
+        name: "baalTemplate",
+        template: "baal-template.yaml",
+      },
+      {
+        name: "yeeterTemplate",
+        template: "yeeterShaman-template.yaml",
+      },
+    ],
   },
   "arbitrum-one": {
     dataSources: [
